3f8d867525bec6746d9cda1ab9fc916621a32a8b
RojasSaenzJulian <105955487+RojasSaenzJulian@users.noreply.github.com>
2023-05-19 21:15:04 -0600

    Test

---
+++ src/main/java/com/TiendaSC_403/TiendaSc403Application.java
@@ -0,0 +1,13 @@
+package com.TiendaSC_403;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaSc403Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaSc403Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/TiendaSC_403/TiendaSc403ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TiendaSC_403;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaSc403ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
